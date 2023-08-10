858585b7423a1f6930b22ab64cf4f4bbaea68201
Matheus438 <matheusbispoa3@gmail.com>
2023-08-10 10:45:36 -0300

    criada a funçao do banco de dados

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioController extends Controller
+{
+    public function store(Request $request){
+        $usuario = Usuario::create([
+            'nome' => $request->nome,
+            'cpf' => $request->cpf,
+            'celular' => $request->celular,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            "succes" => true,
+            "message" => "Usucario cadastrado",
+            "data" => $usuario
+        ], 200);
+    }
+}
